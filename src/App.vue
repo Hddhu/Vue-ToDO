b2d7f17cf2293a6e26205c099d976ddac80d8929
Hanne <hanne.dedeckere@student.hu.nl>
2021-10-12 10:13:58 +0200

    Toggle-add-task toegevoegd

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
-    <AddTask @add-task="addTask" />
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" />
+    <div v-if="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div> 
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
@@ -16,16 +18,21 @@ export default {
   components: {
     Header,
     Tasks,
-    AddTask
+    AddTask,
   },
   data() {
     return {
-      tasks:[]
+      tasks:[],
+      showAddTask: false
     }
   },
   methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+
     addTask(task) {
-      this.task = [...this.task, task]
+      this.tasks = [...this.tasks, task]
     },
 
     deleteTask(id) {
